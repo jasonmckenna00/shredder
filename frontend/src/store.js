9a8d58a90699e67ee732493c69ba8b02c4b623eb
Jason McKenna <jasonmckenna00@gmail.com>
2022-06-06 19:48:46 -0400

    xser to pc

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,26 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
 import rootReducer from './reducers/root_reducer';
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middleware = [thunk]
-const initialState = {}
-const store = createStore(
-  rootReducer,  
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware)))
-  
-  
-  export default store;
+// const middleware = [thunk]
+// const initialState = {}
+// const store = createStore(
+//   rootReducer,  
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware)))
+
+const rootReducer = combineReducers({
+  mountains: mountainReducer,
+  favoriteMountains: favoriteMountainReducer
+})
+
+const store = configureStore({
+  reducer:{
+    rootReducer
+  }
+})
+
+
+
+export default store;
